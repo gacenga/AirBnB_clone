e56734fcb828305fb4fc45c77bd60bd18efbc4c7
Gacenga <jimwillymurigu@gmail.com>
2024-03-07 07:47:06 -0800

    child classes

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+from models.base_model import BaseModel
+import json
+
+class Amenity(BaseModel):
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+"""
+
+from models.base_model import BaseModel
+import json
+
+class City(BaseModel):
+    """ """
+    state_id = ""
+    name = ""
+    
